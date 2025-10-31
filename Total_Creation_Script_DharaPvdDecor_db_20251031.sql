74befa710ce4631f7558a3800deecdc7622a50a8
Priyanka Ayyanagoudar <radiantsmiles2000@gmail.com>
2025-10-31 12:10:59 +0530

    auto update

--- Total_Creation_Script_DharaPvdDecor_db_20251031.sql
+++ Total_Creation_Script_DharaPvdDecor_db_20251031.sql
@@ -1,28 +1,44 @@
 create database DharaPvdDecor_db;
 use DharaPvdDecor_db;
 
+---------------------------------------------------------------
+
+create table errorlog(
+errorlogid int identity(1,1) primary key,
+errornumber int,
+errorprocedure nvarchar(128),
+errorline int ,
+errormessage nvarchar(max),
+);
+
+---------------------------------------------------------------
+
 create table user_mast(
 user_id bigint identity(1,1) primary key,
-user_name varchar(50) not null unique,
-user_password varchar(50) not null unique,
-user_role varchar(20),
+user_name varchar(100) not null unique,
+user_password varchar(100) not null unique,
+user_role varchar(50),
 is_login bit,
 created_date date,
 updated_date date
 );
 
+---------------------------------------------------------------
+
 create table country_mast(
 country_id bigint identity(1,1) primary key,
-country_name Varchar(50) not null unique,
+country_name varchar(100) not null unique,
 created_date date,
 updated_date date,
 user_id bigint
 foreign key(user_id) references user_mast(user_id)
 );
 
+---------------------------------------------------------------
+
 create table state_mast(
 state_id bigint identity(1,1) primary key,
-state_name Varchar(50) not null unique ,
+state_name varchar(100) not null unique ,
 country_id bigint,
 created_date date,
 updated_date date,
@@ -31,10 +47,11 @@ foreign Key(country_id) references country_mast(country_id),
 foreign Key(user_id) references user_mast(user_id)
 );
 
+---------------------------------------------------------------
 
 create table city_mast(
 city_id bigint identity(1,1) primary key,
-city_name Varchar(50) not null unique,
+city_name varchar(100) not null unique,
 state_id bigint,
 created_date date,
 updated_date date,
@@ -43,18 +60,20 @@ foreign key(state_id) references state_mast(state_id),
 foreign key(user_id) references user_mast(user_id)
 );
 
+---------------------------------------------------------------
+
 create table company_mast(
 comp_id bigint identity(1,1) primary key,
 comp_code varchar(25) not null unique,
-comp_name varchar(50) not null unique,
+comp_name varchar(100) not null unique,
 comp_short_name varchar(6) not null unique,
 comp_type varchar(50),
 comp_desc varchar(max),
 cin_number varchar(25),
 gst_number varchar(25),
 pan_number varchar(12),
-contperson_name varchar(50),
-contact_email varchar(50),
+contperson_name varchar(100),
+contact_email varchar(max),
 contact_phone varchar(12),
 address_line1 varchar(max),
 address_line2 varchar(max),
@@ -63,11 +82,13 @@ pincode varchar(6),
 is_active bit,
 created_date date,
 updated_date date,
-logo_path varchar(50),
+logo_path varchar(max),
 user_id bigint,
 foreign key(city_id) references city_mast(city_id),
 foreign key(user_id) references user_mast(user_id)
 )
+
+---------------------------------------------------------------
 
 create table user_details(
 user_details_id bigint identity(1,1) primary key,
@@ -82,6 +103,8 @@ foreign key(comp_id) references Company_Mast(comp_id),
 foreign key(modified_by) references User_Mast(user_id)
 )
 
+---------------------------------------------------------------
+
 create table month_mast(
 month_id bigint identity(1,1) primary key,
 month_name varchar(50) not null,
@@ -93,10 +116,13 @@ user_id bigint
 foreign key(user_id) references user_mast(user_id)
 );
 
+---------------------------------------------------------------
+
+
 create table fin_year_mast(
 fin_year_id bigint identity(1,1) primary key,
 fin_name varchar(50) not null unique,
-short_fin_year varchar(6),
+short_fin_year varchar(10),
 year_start date,
 year_end date,
 created_date date,
@@ -104,6 +130,8 @@ updated_date date,
 user_id bigint
 foreign key(user_id) references user_mast(user_id)
 )
+
+---------------------------------------------------------------
 
 create table emp_calenderdays(
 emp_calender_id bigint identity(1,1) primary key,
@@ -120,6 +148,8 @@ foreign key(month_id) references month_mast(month_id),
 foreign key(user_id) references user_mast(user_id)
 )
 
+---------------------------------------------------------------
+
 create table colour_mast(
 colour_id bigint identity(1,1) primary key,
 colour_name varchar(50) not null unique,
@@ -129,6 +159,8 @@ updated_date date,
 user_id bigint
 foreign key(user_id) references user_mast(user_id)
 )
+
+---------------------------------------------------------------
 
 create table unit_mast(
 unit_id bigint identity(1,1) primary key,
@@ -140,6 +172,9 @@ updated_date date,
 user_id bigint
 foreign key(user_id) references user_mast(user_id)
 )
+
+---------------------------------------------------------------
+
 
 create table hsn_mast(
 hsn_id bigint identity(1,1) primary key,
@@ -153,9 +188,11 @@ user_id bigint
 foreign key(user_id) references user_mast(user_id)
 )
 
+---------------------------------------------------------------
+
 create table prodtype_master(
 prodtype_id bigint identity(1,1) primary key,
-prodtype_name varchar(50),
+prodtype_name varchar(100),
 prodtype_desc varchar(max),
 created_date date,
 updated_date date,
@@ -163,9 +200,11 @@ user_id bigint
 foreign key(user_id) references user_mast(user_id)
 )
 
+---------------------------------------------------------------
+
 create table brand_mast(
 brand_id bigint identity(1,1) primary key,
-brand_name varchar(50) unique,
+brand_name varchar(100) unique,
 brand_desc varchar(max),
 created_date date,
 updated_date date,
@@ -173,9 +212,12 @@ user_id bigint
 foreign key(user_id) references user_mast(user_id)
 )
 
+---------------------------------------------------------------
+
+
 create table paytype_mast(
 paytype_id bigint identity(1,1) primary key,
-paytype_name varchar(50) unique,
+paytype_name varchar(100) unique,
 paytype_desc varchar(max),
 created_date date,
 updated_date date,
@@ -183,15 +225,17 @@ user_id bigint
 foreign key(user_id) references user_Mast(user_id)
 )
 
+---------------------------------------------------------------
+
 create table customer_mast(
 customer_id bigint identity(1,1) primary key,
-customer_name varchar(50) not null unique,
+customer_name varchar(100) not null unique,
 prefix varchar(6),
 gender varchar(10),
 phonenumber varchar(12),
 city_id bigint,
 cust_address varchar(max),
-email_id varchar(50),
+email_id varchar(max),
 dob date,
 aadhaar_number varchar(15),
 license_number varchar(18),
@@ -206,15 +250,17 @@ foreign key(city_id) references city_mast(city_id),
 foreign key(user_id) references user_mast(user_id)
 )
 
+---------------------------------------------------------------
+
 create table vendor_mast(
 vendor_id bigint identity(1,1) primary key,
-vendor_name varchar(50) not null unique,
+vendor_name varchar(100) not null unique,
 prefix varchar(6),
 gender varchar(10),
 phonenumber varchar(12),
 city_id bigint,
 [address] varchar(max),
-email_id varchar(50),
+email_id varchar(max),
 dob date,
 aadhaar_number varchar(15),
 license_number varchar(18),
@@ -229,13 +275,15 @@ foreign key(city_id) references city_mast(city_id),
 foreign key(user_id) references user_mast(user_id)
 )
 
+---------------------------------------------------------------
+
 create table product_mast(
 product_id bigint identity(1,1) primary key,
 prodtype_id bigint,
 brand_id bigint,
 hsn_id bigint,
 unit_id bigint,
-product_name varchar(50) not null unique,
+product_name varchar(100) not null unique,
 product_desc varchar(max),
 rate decimal(12,2),
 opening_stock decimal(10,2),
@@ -254,6 +302,8 @@ foreign key(hsn_id) references hsn_mast(hsn_id),
 foreign key(unit_id) references unit_mast(unit_id),
 foreign Key(user_id) references user_mast(user_id)
 )
+
+---------------------------------------------------------------
 
 create table inward_mast(
 inward_id bigint identity(1,1) primary key,
@@ -274,6 +324,8 @@ foreign key(comp_id) references company_mast(comp_id),
 foreign key(user_id) references user_mast(user_id)
 )
 
+---------------------------------------------------------------
+
 create table inward_return(
 inwardreturn_id bigint identity(1,1) primary key,
 inward_id bigint,
@@ -293,6 +345,8 @@ foreign key(fin_year_id) references fin_year_mast(fin_year_id),
 foreign key(comp_id) references company_mast(comp_id),
 foreign key(user_id) references user_mast(user_id)
 )
+
+---------------------------------------------------------------
 
 create table salesinvoice_mast(
 sales_id bigint identity(1,1) primary key,
@@ -319,6 +373,8 @@ foreign key(fin_Year_id) references fin_year_mast(fin_Year_id),
 foreign key(comp_id) references company_mast(comp_id),
 foreign key(user_id) references user_mast(user_id)
 )
+
+---------------------------------------------------------------
 
 create table salesinvoicedetails(
 sales_detail_id bigint identity(1,1) primary key,
@@ -360,6 +416,8 @@ foreign key(comp_id) references company_mast(comp_id),
 foreign key(user_id) references user_mast(user_id)
 )
 
+---------------------------------------------------------------
+
 create table challan_mast(
 challan_id bigint identity(1,1) primary key,
 sales_id bigint,
@@ -373,6 +431,9 @@ foreign key(fin_year_id) references fin_year_mast(fin_year_id),
 foreign key(comp_id) references company_mast(comp_id),
 foreign key(user_id) references user_mast(user_id)
 )
+
+---------------------------------------------------------------
+
 
 create table receipt_mast(
 receipt_id bigint identity(1,1) primary key,
@@ -392,13 +453,16 @@ foreign key(comp_id) references company_mast(comp_id),
 foreign key(user_id) references user_mast(user_id)
 )
 
+---------------------------------------------------------------
+
+
 create table receipt_details(
 receipt_dtl_id  bigint identity(1,1) primary key,
 receipt_id bigint,
 paytype_id bigint,
-total_Amt decimal(12,2),
+total_amt decimal(12,2),
 cheque_number varchar(10),
-cheque_bankname varchar(50),
+cheque_bankname varchar(100),
 ifsc_code varchar(10),
 cheque_date date,
 account_number varchar(25),
@@ -416,6 +480,9 @@ foreign key(fin_year_id) references fin_year_mast(fin_year_id),
 foreign key(comp_id) references company_mast(comp_id),
 foreign key(user_id) references user_Mast(user_id)
 )
+
+---------------------------------------------------------------
+
 
 create table purchaseinvoice_mast(
 purchase_id bigint identity(1,1) primary key,
@@ -443,6 +510,9 @@ foreign key(fin_year_id) references fin_year_mast(fin_year_id),
 foreign key(comp_id) references company_mast(comp_id),
 foreign key(user_id) references user_mast(user_id)
 )
+
+---------------------------------------------------------------
+
 
 create table purchaseinvoice_details(
 purchase_detail_id bigint identity(1,1) primary key,		
@@ -482,9 +552,12 @@ foreign key(comp_id) references company_mast(comp_id),
 foreign key(user_id) references user_mast(user_id)
 )
 
+---------------------------------------------------------------
+
+
 create table trans_type_mast(
 trans_id bigint identity(1,1) primary key,
-transtype_name	varchar(50),	
+transtype_name	varchar(100),	
 transtype_desc	varchar(max),	
 created_date date,	
 updated_date date,		
@@ -493,6 +566,7 @@ foreign key (user_id) references user_mast(user_id)
 )
 
 ----------------------------------------------------------------------------------------------------------
+
 create table dailyconsumption_mast(
 dailycons_id bigint identity(1,1) primary key,
 dailycons_date date,
@@ -512,9 +586,11 @@ foreign key(comp_id) references company_mast(comp_id),
 foreign key(user_id) references user_Mast(user_id)
 )
 
+---------------------------------------------------------------
+
 create table employee_desg_mast(
 desg_id bigint identity(1,1) primary key,
-desg_name varchar(50),
+desg_name varchar(100),
 desg_desc varchar(max),
 daily_wk_hr decimal(5,2), 
 created_date date,
@@ -523,19 +599,22 @@ user_id bigint
 foreign key(user_id) references user_mast(user_id)
 )
 
+---------------------------------------------------------------
+
+
 create table employee_mast(
 employee_id bigint identity(1,1) primary key,
 desg_id bigint,
-first_name varchar(50), 
-last_name varchar(50),
+first_name varchar(100), 
+last_name varchar(100),
 gender varchar(10),
 dob date,  
 phone_number varchar(12),  
-emailid varchar(50),
+emailid varchar(max),
 [address] varchar(max), 
 city_id bigint,
 aadhaar_number varchar(15),
-pan_number Varchar(12),
+pan_number varchar(12),
 bankaccount_no  varchar(20),
 ifsc_code varchar(8),
 joining_date  date,
@@ -556,11 +635,14 @@ foreign key(comp_id) references company_mast(comp_id),
 foreign key(user_id) references user_mast(user_id)
 )
 
+---------------------------------------------------------------
+
+
 create table leavetype_mast(
 leavetype_id bigint identity(1,1) primary key,
 yearsincompany decimal(5,2),
 allocated_leaves decimal(5,2),
-leave_name varchar(50),
+leave_name varchar(100),
 leave_desc varchar(max),
 casual_leaves decimal(5,2),
 sick_leaves decimal(5,2),
@@ -569,6 +651,9 @@ updated_date date,
 user_id bigint
 foreign key(user_id) references user_mast(user_id)
 )
+
+---------------------------------------------------------------
+
 
 create table emp_leave_mast(
 emp_leave_id bigint identity(1,1) primary key,
@@ -590,6 +675,8 @@ foreign key(leavetype_Id) references leavetype_mast(leavetype_id),
 foreign key(user_id) references user_mast(user_id)
 )
 
+---------------------------------------------------------------
+
 
 create table employee_payslip(
 emp_payslip_id bigint identity(1,1) primary key,
@@ -604,7 +691,7 @@ actual_working_hours decimal(5,2),
 monthly_salary decimal(12,2),
 hourly_salary decimal(12,2),
 over_time_hours decimal(5,2),
-over_time_Amount decimal(12,2),
+over_time_amount decimal(12,2),
 gross_amount decimal(12,2),
 pf_amount decimal(12,2),
 tds_amount decimal(12,2),
@@ -621,6 +708,9 @@ foreign key(employee_id) references employee_mast(employee_id),
 foreign key(emp_calender_id) references emp_calenderdays(emp_calender_id),
 foreign key(user_id) references user_mast(user_id)
 )
+
+---------------------------------------------------------------
+
 
 create table payment_mast(
 payment_id bigint identity(1,1) primary key,
@@ -650,9 +740,9 @@ create table payment_details(
 payment_detail_id bigint identity(1,1) primary key,	
 payment_id bigint,				
 pay_type_id	 bigint,			
-total_amount decimal(12,2),		
+total_amt decimal(12,2),		
 cheque_number varchar(20),		
-cheque_bankname varchar(50),	
+cheque_bankname varchar(100),	
 ifsc_code varchar(11),			
 cheque_date	date,	
 account_number varchar(20),	
@@ -671,15 +761,6 @@ foreign key (fin_year_id) references fin_year_mast(fin_year_id),
 foreign key (user_id) references user_mast(user_id)
 )
 
-CREATE TABLE [dbo].[errorlog](
-	[errorlogid] [int] IDENTITY(1,1) NOT NULL,
-	[error_number] [int] NULL,
-	[error_procedure] [nvarchar](128) NULL,
-	[error_line] [int] NULL,
-	[error_message] [nvarchar](max) NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[errorlogid] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
+---------------------------------------------------------------
+
+
