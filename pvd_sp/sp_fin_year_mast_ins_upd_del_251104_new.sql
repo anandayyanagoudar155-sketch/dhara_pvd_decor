2f58395e2c259d4708ce8faaaaefa64327f3bedc
Priyanka Ayyanagoudar <radiantsmiles2000@gmail.com>
2025-11-04 17:28:42 +0530

    auto update

---
+++ pvd_sp/sp_fin_year_mast_ins_upd_del_251104_new.sql
@@ -0,0 +1,231 @@
+USE [DharaPvdDecor_db]
+GO
+
+/****** Object:  StoredProcedure [dbo].[sp_fin_year_mast_ins_upd_del]    Script Date: 04-11-2025 17:13:11 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+CREATE procedure [dbo].[sp_fin_year_mast_ins_upd_del]
+(
+@action varchar(max) = '',
+@fin_year_id bigint = 0,
+@fin_name varchar(50) = '',
+@short_fin_year varchar(10) = '',
+@year_start date = null,
+@year_end date = null,
+@created_date date = null,
+@updated_date date = null,
+@user_id bigint = null
+)
+as
+BEGIN
+declare @errornumber int, @errorprocedure nvarchar(128), @errorline int, @errormessage nvarchar(max);
+	if @action = 'insert'
+	begin
+		begin try
+			begin transaction
+			insert into fin_year_mast(fin_name,short_fin_year,year_start,year_end,created_date,updated_date,user_id)
+			values(@fin_name,@short_fin_year,@year_start,@year_end,@created_date,@updated_date,@user_id);
+			commit transaction;
+		end try
+		begin catch
+			If XACT_STATE() <> 0
+				rollback transaction;
+
+			set @errornumber = ERROR_NUMBER();
+			set @errorprocedure = ERROR_PROCEDURE();
+			set @errorline = ERROR_LINE();
+			set @errormessage = ERROR_MESSAGE();
+
+			exec errorlog_ins 
+				 @errornumber,
+				 @errorprocedure,
+				 @errorline,
+				 @errormessage;	
+
+			THROW;
+		end catch
+	end
+	if @action = 'select all'
+	begin
+		begin try
+			select fm.fin_year_id,fm.fin_name,fm.short_fin_year,fm.year_start,fm.year_end,
+			fm.created_date,fm.updated_date,um.user_name
+			from fin_year_mast fm
+			inner join user_mast um on fm.user_id = um.user_id;
+		end try
+		begin catch
+			set @errornumber = ERROR_NUMBER();
+			set @errorprocedure = ERROR_PROCEDURE();
+			set @errorline = ERROR_LINE();
+			set @errormessage = ERROR_MESSAGE();
+
+			exec errorlog_ins 
+				 @errornumber,
+				 @errorprocedure,
+				 @errorline,
+				 @errormessage;
+				 
+			THROW;
+		end catch
+	end
+	if @action = 'select one'
+	begin
+		begin try
+			select fm.fin_year_id,fm.fin_name,fm.short_fin_year,fm.year_start,fm.year_end,
+			fm.created_date,fm.updated_date,um.user_name
+			from fin_year_mast fm
+			inner join user_mast um on fm.user_id = um.user_id
+			where fm.fin_year_id = @fin_year_id;
+		end try
+		begin catch
+			set @errornumber = ERROR_NUMBER();
+			set @errorprocedure = ERROR_PROCEDURE();
+			set @errorline = ERROR_LINE();
+			set @errormessage = ERROR_MESSAGE();
+
+			exec errorlog_ins 
+				 @errornumber,
+				 @errorprocedure,
+				 @errorline,
+				 @errormessage;	
+
+			THROW;
+		end catch
+	end
+	if @action = 'delete'
+	begin
+		begin try
+			 begin transaction
+			 declare @rec_count bigint;
+
+			 set @rec_count = (
+									Select sum(cnt) from
+									(
+										Select count(fin_year_id) as cnt from emp_calenderdays where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from inward_mast where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from inward_return where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from salesinvoice_mast where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from  salesinvoicedetails where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from challan_mast where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from receipt_mast where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from receipt_details where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from purchaseinvoice_mast where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from PurchaseInvoice_Details where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from payment_mast where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from payment_details where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from dailyconsumption_mast where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from employee_mast where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from emp_leave_mast where fin_year_id=@fin_year_id
+										union all
+										Select count(fin_year_id) as cnt from employee_payslip where fin_year_id=@fin_year_id 
+								    ) as rec_count
+							  );
+			if @rec_count>0
+			begin
+				rollback transaction;
+				return;
+			end
+
+			delete from fin_year_mast where fin_year_id = @fin_year_id;
+			commit transaction;
+		end try
+		begin catch
+			if XACT_STATE() <> 0
+				rollback transaction;
+
+			set @errornumber = ERROR_NUMBER();
+			set @errorprocedure = ERROR_PROCEDURE();
+			set @errorline = ERROR_LINE();
+			set @errormessage = ERROR_MESSAGE();
+
+			exec errorlog_ins 
+				 @errornumber,
+				 @errorprocedure,
+				 @errorline,
+				 @errormessage;	
+
+			THROW;
+		end catch
+	end
+	if @action = 'update'
+	begin
+		begin try
+			begin transaction;
+			update fin_year_mast
+			set fin_name=@fin_name,
+				short_fin_year=@short_fin_year,
+				year_start=@year_start,
+				year_end=@year_end,
+				updated_date=@updated_date,
+				user_id = @user_id
+			where fin_year_id = @fin_year_id;
+			
+			if @@ROWCOUNT = 0
+			begin
+				rollback transaction;
+				return;
+			end
+			commit transaction;
+		end try
+		begin catch
+			if XACT_STATE() <> 0
+				rollback transaction;
+
+			set @errornumber = ERROR_NUMBER();
+			set @errorprocedure = ERROR_PROCEDURE();
+			set @errorline = ERROR_LINE();
+			set @errormessage = ERROR_MESSAGE();
+
+			exec errorlog_ins 
+				 @errornumber,
+				 @errorprocedure,
+				 @errorline,
+				 @errormessage;	
+
+			THROW;
+		end catch
+	end
+	if @action = 'fin_year_mastlist'
+	begin
+		begin try
+			select fin_year_id,fin_name,short_fin_year from fin_year_mast;
+		end try
+		begin catch
+			set @errornumber = ERROR_NUMBER();
+			set @errorprocedure = ERROR_PROCEDURE();
+			set @errorline = ERROR_LINE();
+			set @errormessage = ERROR_MESSAGE();
+
+			exec errorlog_ins 
+				 @errornumber,
+				 @errorprocedure,
+				 @errorline,
+				 @errormessage;	
+
+			THROW;
+		end catch
+	end
+END
+GO
+
---
+++ pvd_sp/sp_month_mast_ins_upd_del_251104_new.sql
@@ -0,0 +1,203 @@
+USE [DharaPvdDecor_db]
+GO
+
+/****** Object:  StoredProcedure [dbo].[sp_month_mast_ins_upd_del]    Script Date: 04-11-2025 17:13:42 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+
+CREATE procedure [dbo].[sp_month_mast_ins_upd_del]
+(
+@action varchar(max) = '',
+@month_id bigint = 0,
+@month_name varchar(50) = '',
+@start_date date = null,
+@end_date date = null,
+@created_date date = null,
+@updated_date date = null,
+@user_id bigint = 0
+)
+as
+BEGIN
+declare @errornumber int, @errorprocedure nvarchar(128), @errorline int, @errormessage nvarchar(max);
+	if @action = 'insert'
+	begin
+		begin try
+			begin transaction
+			insert into month_mast(month_name,start_date,end_date,created_date,updated_date,user_id)
+			values(@month_name,@start_date,@end_date,@created_date,@updated_date,@user_id);
+			commit transaction;
+		end try
+		begin catch
+			if XACT_STATE() <> 0
+				rollback transaction;
+			
+			set @errornumber = ERROR_NUMBER();
+			set @errorprocedure = ERROR_PROCEDURE();
+			set @errorline = ERROR_LINE();
+			set @errormessage = ERROR_MESSAGE();
+
+			exec errorlog_ins 
+				 @errornumber,
+				 @errorprocedure,
+				 @errorline,
+				 @errormessage;
+
+			THROW;
+		end catch
+	end
+	if @action = 'select all'
+	begin
+		begin try
+			Select mm.month_id,mm.month_name,mm.start_date,mm.end_date,mm.created_date,mm.updated_date,um.user_name  
+			from month_mast mm
+			inner join user_mast um on mm.user_id=um.user_id;
+		end try
+		begin catch
+			set @errornumber = ERROR_NUMBER();
+			set @errorprocedure = ERROR_PROCEDURE();
+			set @errorline = ERROR_LINE();
+			set @errormessage = ERROR_MESSAGE();
+
+			exec errorlog_ins 
+				 @errornumber,
+				 @errorprocedure,
+				 @errorline,
+				 @errormessage;
+
+			THROW;
+		end catch
+	end
+	if @action = 'select one'
+	begin
+		begin try
+			Select mm.month_id,mm.month_name,mm.start_date,mm.end_date,mm.created_date,mm.updated_date,um.user_name  
+			from month_mast mm
+			inner join user_mast um on mm.user_id=um.user_id
+			where mm.month_id=@month_id
+		end try
+		begin catch
+			set @errornumber = ERROR_NUMBER();
+			set @errorprocedure = ERROR_PROCEDURE();
+			set @errorline = ERROR_LINE();
+			set @errormessage = ERROR_MESSAGE();
+
+			exec errorlog_ins 
+				 @errornumber,
+				 @errorprocedure,
+				 @errorline,
+				 @errormessage;
+
+			THROW;
+		end catch
+	end
+	if @action = 'delete'
+	begin 
+	declare @rec_count bigint;
+		begin try
+			begin transaction
+			set @rec_count = (
+								Select sum(cnt) from
+								(
+									Select count(month_id) as cnt from emp_calenderdays where month_id=@month_id
+									union all
+									Select count(month_id) as cnt from emp_leave_mast where month_id=@month_id
+									union all
+									Select count(month_id) as cnt from employee_payslip where month_id=@month_id
+								) as rec_count
+							 );
+			if @rec_count > 0
+			begin
+				rollback transaction;
+				return;
+			end
+			delete from month_mast where month_id = @month_id;
+			commit transaction;
+		end try
+		begin catch
+			If XACT_STATE() <> 0
+				rollback transaction;
+
+			set @errornumber = ERROR_NUMBER();
+			set @errorprocedure = ERROR_PROCEDURE();
+			set @errorline = ERROR_LINE();
+			set @errormessage = ERROR_MESSAGE();
+
+			exec errorlog_ins 
+				 @errornumber,
+				 @errorprocedure,
+				 @errorline,
+				 @errormessage;
+
+			THROW;
+		end catch
+	end
+	if @action = 'update'
+	begin
+		begin try
+			begin transaction
+			update month_mast
+			set month_name = @month_name,
+				start_date = @start_date,
+				end_date = @end_date,
+				updated_date = @updated_date,
+				@user_id = @user_id
+			where month_id = @month_id;
+
+			if @@ROWCOUNT = 0
+			 begin
+				rollback transaction;
+				return;
+			 end
+
+			commit transaction;
+		end try
+		begin catch
+		 If XACT_STATE() <> 0 
+			rollback transaction;
+
+		set @errornumber = ERROR_NUMBER();
+		set @errorprocedure = ERROR_PROCEDURE();
+		set @errorline = ERROR_LINE();
+		set @errormessage = ERROR_MESSAGE();
+
+		exec errorlog_ins 
+			@errornumber,
+			@errorprocedure,
+			@errorline,
+			@errormessage;
+
+		THROW;
+		end catch
+	end
+	if @action = 'month_mastlist'
+	begin
+		begin try
+			select month_id,month_name
+			from month_mast;
+		end try
+		begin catch
+			set @errornumber = ERROR_NUMBER();
+			set @errorprocedure = ERROR_PROCEDURE();
+			set @errorline = ERROR_LINE();
+			set @errormessage = ERROR_MESSAGE();
+
+			exec errorlog_ins 
+				@errornumber,
+				@errorprocedure,
+				@errorline,
+				@errormessage;
+
+			THROW;
+		end catch
+	end 
+END
+GO
+
